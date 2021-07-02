7eda6d07f6599e5292e2746e771f4f5efe2a2677
Juan <jcorlandomc@yahoo.com>
2021-07-02 14:47:25 -0700

    2021-07-2 2:47 PM

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@
 int main()
 {
     vector3D<double> vec1(3.3, 8.5, 7.7);
-    
+    vector3D<double> vec2;
+
+    std::cout << vec1[0] << std::endl;
+    std::cout << vec1[1] << std::endl;
+    std::cout << vec1[2] << std::endl;
+
+    std::cout << vec2[0] << std::endl;
+    std::cout << vec2[1] << std::endl;
+    std::cout << vec2[2] << std::endl;
+
     return 0;
 }
 
--- vector.hpp
+++ vector.hpp
@@ -3,16 +3,131 @@
 #include <array>
 
 
+
+
+
+
+
+
+
+
+
+
+// vv<<<--- This is the Class Template
 template<class T>
 class vector3D
 {
 private:
+    // Member variables
     std::array<T, 3> vectr;
 public:
-    vector3D() = default;
+    // Constructors
+    vector3D();
     vector3D(T a, T b, T c);
+    // Overloading [] operator to access elements in array style
+    // v<<-- Non-Const Version
+    T &operator [](int  index);
+    // v<<-- Const Version
+    const T &operator [](int  index) const;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vv<<<----- This is for Non-Const Objects
+// Implementation of [] operator.  This function must return
+// a reference as array element can be put on left side
+template<class T>
+T &vector3D<T>::operator [](int  index)
+{
+    return vectr[index];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vv<<<----- This is for Const Objects
+template<class T>
+const T &vector3D<T>::operator [](int  index) const
+{
+    return vectr[index];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vv<<<---- Constructor definition
+template<class T>
+vector3D<T>::vector3D()
+{
+    vectr[0] = 0;
+    vectr[1] = 0;
+    vectr[2] = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vv<<<---- Constructor With Arguments definition
 template<class T>
 vector3D<T>::vector3D(T a, T b, T c)
 {
@@ -20,6 +135,20 @@ vector3D<T>::vector3D(T a, T b, T c)
     vectr[1] = b;
     vectr[2] = c;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif // VECTOR_HPP
